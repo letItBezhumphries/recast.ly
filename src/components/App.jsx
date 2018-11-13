3ac3abf2f42f1540d8dc030e8a68d91a9af273e3
Jason Bourget <31008618+jason-bourget@users.noreply.github.com>
2018-11-12 19:42:30 -0700

    working on activeVideo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,32 @@
-var App = (props) => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: this.props.videos,
+      currentVideo: this.props.videos[0],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.props.videos[0]}/>
+          </div>
+          <div className="col-md-5">
+            <VideoList videos ={this.props.videos}/>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer video={props.videos[0]}/>
-      </div>
-      <div className="col-md-5">
-        <VideoList videos = {props.videos}/>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
