2a8f798fbbbc89ea525275ca6e83ee1ef9d2b874
EricZH <letItBezhumphries>
2018-11-13 23:45:17 -0800

    Complete creating reusable API helper

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.onVideoClick = this.onVideoClick.bind(this);
+    this.onYouTubeQuerySubmit = this.onYouTubeQuerySubmit.bind(this);
     this.state = {
       videos: window.exampleVideoData,
       currentVideo: window.exampleVideoData[0],
     };
-    this.onVideoClick = this.onVideoClick.bind(this);
+    
   }
 
   onVideoClick(video) {
@@ -14,12 +16,25 @@ class App extends React.Component {
     });
   }
 
+  // onYouTubeQuerySubmit(query) {
+  //   var value = {
+  //     query: query
+  //   };
+  //   this.props.search(value, (videos) => {
+  //     this.setState({
+  //       video: videos[0],
+  //       videos: videos
+  //     });
+  //   });
+  // }
+
+
   render() {
     return (
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div><h5><em>search</em> view goes here</h5></div>
+            <div> <Search search={this.onYouTubeQuerySubmit}/> </div>
           </div>
         </nav>
         <div className="row">
@@ -27,7 +42,7 @@ class App extends React.Component {
             <VideoPlayer video={this.state.currentVideo}/>
           </div>
           <div className="col-md-5">
-            <VideoList videos = {this.state.videos} onVideoClick = {this.onVideoClick}/>
+            <VideoList videos={this.state.videos} onVideoClick={this.onVideoClick}/>
           </div>
         </div>
       </div>
