32c5f8d687eae149378016d36b7bc7f7a25a8907
EricZH <letItBezhumphries>
2018-11-14 23:31:29 -0800

    fixing issues with App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,25 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.onVideoClick = this.onVideoClick.bind(this);
-    this.onYouTubeQuerySubmit = this.onYouTubeQuerySubmit.bind(this);
     this.state = {
       videos: window.exampleVideoData,
-      currentVideo: window.exampleVideoData[0],
-      value: ''
+      currentVideo: this.exampleVideoData[0],
     };
-    
+    this.onVideoClick = this.onVideoClick.bind(this);
   }
 
-  onVideoClick(video) {
+  onVideoClick() {
     this.setState({
-      currentVideo: video
+      currentVideo: video 
     });
   }
-
-  // onYouTubeQuerySubmit(query) {
-  //   var value = {
-  //     query: query
-  //   };
-  //   this.props.search(value, (videos) => {
-  //     this.setState({
-  //       video: videos[0],
-  //       videos: videos
-  //     });
-  //   });
-  // }
-
 
   render() {
     return (
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div> <input onChange={event => this.setState({ value: event.target.value })} /> 
-            Value of the input: {this.state.value}  
-            </div>
+            <div> <Search /> </div>
           </div>
         </nav>
         <div className="row">
@@ -45,15 +27,14 @@ class App extends React.Component {
             <VideoPlayer video={this.state.currentVideo}/>
           </div>
           <div className="col-md-5">
-            <VideoList videos={this.state.videos} onVideoClick={this.onVideoClick}/>
+            <VideoList videos={this.props.videos} onVideoClick={currentVideo => this.setState({currentVideo})}/>
           </div>
         </div>
       </div>
     );
   }
 }
-// comment for commit
+
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
-
 window.App = App;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,6 @@
 var Search = (props) => (
   <div className="search-bar form-inline">
-    <input className="form-control" type="text" onChange={event => console.log(event.target.value)} />
+    <input className="form-control" type="text" />
     <button className="btn hidden-sm-down">
       <span className="glyphicon glyphicon-search"></span>
     </button>
